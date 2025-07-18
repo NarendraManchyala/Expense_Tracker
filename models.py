80667261c98b6e8e705d0dbeb6876c7f44b78aad
Narendra Manchyala <narendramanchyala@gmail.com>
2025-07-18 15:33:12 +0530

    Expense Tracker Site

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+from flask import Flask
+
+db = SQLAlchemy()
+
+
+
+class Transaction(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(10), nullable=False)
+    description = db.Column(db.String(200))
+    date = db.Column(db.Date)
+
+class Trip(db.Model):
+
+    trip_id = db.Column(db.Integer, primary_key = True)
+    trip_name = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100),nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    budget = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200))
